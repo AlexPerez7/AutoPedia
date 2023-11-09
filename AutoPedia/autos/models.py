e48acc4aa4620415cd62efd8c56936216178936d
Alex Pérez Argomedo <64103251+AlexPerez7@users.noreply.github.com>
2023-11-09 18:08:34 +0000

    App Autos

--- AutoPedia/autos/models.py
+++ AutoPedia/autos/models.py
@@ -1,8 +1,12 @@
+# autos/models.py
 from django.db import models
 
 
 class Marca(models.Model):
     nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
 
 
 class Modelo(models.Model):
@@ -11,3 +15,10 @@ class Modelo(models.Model):
     año = models.PositiveIntegerField()
     descripcion = models.TextField()
     imagen = models.ImageField(upload_to="autos/images/")
+    cilindrada_motor = models.DecimalField(
+        max_digits=5, decimal_places=2, null=True, blank=True
+    )
+    configuracion_motor = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.marca} - {self.nombre} ({self.año})"
--- AutoPedia/autos/views.py
+++ AutoPedia/autos/views.py
@@ -1,15 +1,3 @@
 from django.shortcuts import render
-from .models import Marca, Modelo
 
-
-def home(request):
-    return render(request, 'autos/home.html')
-
-def lista_marcas(request):
-    marcas = Marca.objects.all()
-    return render(request, "autos/lista_marcas.html", {"marcas": marcas})
-
-
-def lista_modelos(request, marca_id):
-    modelos = Modelo.objects.filter(marca_id=marca_id)
-    return render(request, "autos/lista_modelos.html", {"modelos": modelos})
+# Create your views here.
